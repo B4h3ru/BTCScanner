0862793f4f55cad83f5285389dcf0c2135141b1c
b4h3ru <Bahiruwalelign@gmail.com>
2026-03-12 02:55:52 -0900

    update number of process

--- btcScanner.py
+++ btcScanner.py
@@ -7,6 +7,7 @@ from bitcoinlib.mnemonic import Mnemonic
 from bitcoinlib.services.services import Service
 import random
 from addressList.addresses import addresses
+from database import create_table, insert_wallet_data
 
 
 def generate_random_32byte_array():
@@ -71,12 +72,13 @@ def search_value(value, data_set)->bool:
 def scanner():
     while(True):
     
-        all_data_set = load_file_to_set("BTCAddressList.txt")
+        all_data_set = load_file_to_set("addressList/BTCAddressList.txt")
             
         private_key_array = generate_random_32byte_array() # [97, 111, 222, 17, 231, 209, 215, 255, 49, 82, 8, 210, 125, 6, 89, 120, 221, 206, 102, 76, 154, 6, 197, 12, 215, 110, 13, 15, 124, 44, 146, 253] 
         private_key_hex = ''.join(f'{b:02x}' for b in private_key_array) #convert 32byte array to hex string
         wallet = generate_random_wallet(private_key_hex)
         
+       
         if search_value(wallet['address'], all_data_set):
             with open("foundAddress.txt","a") as file:
                 file.write(
@@ -89,11 +91,25 @@ def scanner():
                     # + "Balance : " + str(balance) + " BTC\n"
                     + "Search : " + "BTC Address is found"+ "\n"
                     + "---------------------congratulation-------------------------------\n")
+                
+                # Store data in database
+            print("Initializing database...")
+            if not create_table():
+                print("Warning: Database initialization failed. Scanner will continue without database storage.")
+                
+            network_name = Address(wallet['address']).network.name
+            insert_wallet_data(
+                wallet['private_key'],
+                wallet['private_key_hex'],
+                wallet['public_key'],
+                wallet['address'],
+                network_name
+            )
             print("congratulation BTC Addres is found")
             print("The Address is : ",wallet['address'])
         else:
             print("Not Found")
-            # print("Private Key (WIF):", wallet['private_key'])
+            print("Private Key (WIF):", wallet['private_key'])
             # # print("Private Key (hex):", wallet['private_key_hex'])
             # # print("Public Key:", wallet['public_key'])
             # print("BTC Address  : ",wallet['address'])
